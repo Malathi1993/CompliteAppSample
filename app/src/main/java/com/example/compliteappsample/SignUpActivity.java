7717478604d3b878631cf496b19793cde10bb852
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-06-10 19:00:21 +0530

    Add

--- app/src/main/java/com/example/compliteappsample/SignUpActivity.java
+++ app/src/main/java/com/example/compliteappsample/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.example.compliteappsample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.SpannableString;
@@ -10,16 +11,20 @@ import android.text.Spanned;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SignUpActivity extends AppCompatActivity {
 TextView Service;
+Button SignUp;
     String text = "BySlgningUp You Agree to our Product Services";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         Service=(TextView)findViewById(R.id.textView2);
+        SignUp=(Button) findViewById(R.id.button);
         // initialize a new SpannableStringBuilder instance
         SpannableStringBuilder ssBuilder = new SpannableStringBuilder(text);
 
@@ -32,5 +37,14 @@ TextView Service;
         );
 
         Service.setText(ssBuilder);
-    }
+
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(it);
+            }
+});
 }
+}
+
